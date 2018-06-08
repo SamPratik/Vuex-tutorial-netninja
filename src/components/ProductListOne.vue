dbdb801d0c04aa5fe8e47e0f8a67ebd21a53c43f
SamPratik <pratik.anwar@gmail.com>
2018-06-08 15:09:52 +0600

    actions

--- src/components/ProductListOne.vue
+++ src/components/ProductListOne.vue
@@ -7,7 +7,7 @@
             <span class="price">£{{ product.price }}</span>
         </li>
     </ul>
-    <button v-on:click="reducePrice">Reduce Price</button>
+    <button v-on:click="reducePrice(4)">Reduce Price</button>
   </div>
 </template>
 <script>
@@ -18,8 +18,8 @@ export default {
     }
   },
   methods: {
-    reducePrice() {
-      this.$store.commit('reducePrice');
+    reducePrice(amount) {
+      this.$store.dispatch('reducePrice', {amount});
     }
   }
 }
